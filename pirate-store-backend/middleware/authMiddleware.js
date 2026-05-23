76d9445c2a03ab3cc77791d918b7c311abc00254
khoder-hammoud <khoderhammoud43@gmail>
2026-05-23 17:23:34 +0300

    bilal

--- pirate-store-backend/middleware/authMiddleware.js
+++ pirate-store-backend/middleware/authMiddleware.js
@@ -4,7 +4,7 @@ const User = require('../models/User');
 // حماية المسارات - فقط المستخدمين المسجلين
 const protect = async (req, res, next) => {
   let token;
-
+// this is unstaged 
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
